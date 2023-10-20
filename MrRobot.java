83ebe74a005b6916e814327346aba3eae6b36cbf
Farid <makflay85zet@gmail.com>
2023-10-20 16:38:22 +0400

    name-changed-MrRobot2

--- MrRobot.java
+++ MrRobot.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 import java.text.SimpleDateFormat;
 
-public class mr_robot {
+public class MrRobot {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
